30eacfb6b06e29b658a4e8094b03d1fb034b1f86
8cy <50817549+8cy@users.noreply.github.com>
2020-05-21 07:40:16 -0700

    move to package name 002

---
+++ dist/app.js
@@ -0,0 +1,17 @@
+"use strict";
+const zerotwo = () => {
+    const quotes = [
+        'I think I have taken a liking to you. Won\'t you be my darling?',
+        'I like the look in your eyes. It makes my heart race. You are now my darling!',
+        'Wow, your taste makes my heart race. It bites and lingers... The taste of danger. You are now my darling!',
+        'Once we die, we\ll only be a statistic. It won\'t matter what we were called.',
+        'Don\'t worry, we\'ll always be together. Until the day we die.',
+        'The weak ones die, big deal.',
+        'If you have anything you wanna say, you better spit it out while you can. Because you\'re all going to die sooner or later.',
+        'It\'s been a long time since I last saw a human cry.',
+    ];
+    let quoteNum = quotes[Math.floor(Math.random() * quotes.length)];
+    return quoteNum;
+};
+module.exports = zerotwo;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2FwcC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsTUFBTSxPQUFPLEdBQUcsR0FBRyxFQUFFO0lBQ2pCLE1BQU0sTUFBTSxHQUFHO1FBQ1gsaUVBQWlFO1FBQ2pFLCtFQUErRTtRQUMvRSwyR0FBMkc7UUFDM0csK0VBQStFO1FBQy9FLGdFQUFnRTtRQUNoRSw4QkFBOEI7UUFDOUIsNkhBQTZIO1FBQzdILHNEQUFzRDtLQUN6RCxDQUFBO0lBQ0QsSUFBSSxRQUFRLEdBQUcsTUFBTSxDQUFDLElBQUksQ0FBQyxLQUFLLENBQUMsSUFBSSxDQUFDLE1BQU0sRUFBRSxHQUFHLE1BQU0sQ0FBQyxNQUFNLENBQUMsQ0FBQyxDQUFBO0lBQ2hFLE9BQU8sUUFBUSxDQUFDO0FBQ3BCLENBQUMsQ0FBQTtBQUNELGlCQUFTLE9BQU8sQ0FBQyJ9
